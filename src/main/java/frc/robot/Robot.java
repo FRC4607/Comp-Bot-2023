cd83476c2beab11ae0b6043ae7b032c6a3a6db64
Tim Middleton <middletontimothy53@gmail.com>
2023-01-21 10:21:23 -0600

    Added temp entry and better calibration to IMU

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,82 +8,102 @@ import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.lib.ADIS16470;
 
+/**
+ * The Robot Class that contains all the code.
+ */
 public class Robot extends TimedRobot {
-  private DoubleLogEntry m_gyroLogEntry;
- 
-  private ADIS16470 m_gyro = new ADIS16470();
- 
-  private Command m_autonomousCommand;
+    private DoubleLogEntry m_gyroAnglLog;
+    private DoubleLogEntry m_gyroTempLog;
 
-  private RobotContainer m_robotContainer;
+    private ADIS16470 m_gyro = new ADIS16470();
 
-  @Override
-  public void robotInit() {
-    m_robotContainer = new RobotContainer();
-    DataLogManager.start();
-    DataLog log = DataLogManager.getLog();
-    m_gyroLogEntry = new DoubleLogEntry(log, "Gryo Angle (deg)");
-    m_gyroLogEntry.append(m_gyro.getAngle());
-  }
+    // private Command m_autonomousCommand;
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-    double gyroAngle = m_gyro.getAngle();
-    m_gyroLogEntry.append(gyroAngle);
-    SmartDashboard.putNumber("Gyro Angle", gyroAngle);
-  }
+    // private RobotContainer m_robotContainer;
 
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    @Override
+    public void robotInit() {
+        // m_robotContainer = new RobotContainer();
+        DataLogManager.start();
+        DataLog log = DataLogManager.getLog();
+        m_gyroAnglLog = new DoubleLogEntry(log, "Gryo Angle (deg)");
+        m_gyroTempLog = new DoubleLogEntry(log, "Gryo Temp (deg C)");
+        m_gyroAnglLog.append(m_gyro.getAngle());
+        m_gyroTempLog.append(m_gyro.getTemp());
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+        double gyroAngle = m_gyro.getAngle();
+        double gyroTemp = m_gyro.getTemp();
+        m_gyroAnglLog.append(gyroAngle);
+        m_gyroTempLog.append(gyroTemp);
+        SmartDashboard.putNumber("Gyro Angle", gyroAngle);
+        SmartDashboard.putNumber("Gyro Temp", gyroTemp);
+        SmartDashboard.putNumber("Time", Timer.getFPGATimestamp());
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void disabledInit() {
+    }
 
-  @Override
-  public void teleopExit() {}
+    @Override
+    public void disabledPeriodic() {
+    }
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledExit() {
+    }
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void autonomousInit() {
+        // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-  @Override
-  public void testExit() {}
+        // if (m_autonomousCommand != null) {
+        //     m_autonomousCommand.schedule();
+        // }
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void autonomousExit() {
+    }
+
+    @Override
+    public void teleopInit() {
+        // if (m_autonomousCommand != null) {
+        //     m_autonomousCommand.cancel();
+        // }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void teleopExit() {
+    }
+
+    @Override
+    public void testInit() {
+        // CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void testExit() {
+    }
 }
